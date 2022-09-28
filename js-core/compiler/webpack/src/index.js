350099be6f115ab60628afd9e9e1b4c59fc7243e
HieuLuu <hieu.luu@binance.com>
2022-09-28 15:41:58 +0700

    feat: asset management webpack

--- js-core/compiler/webpack/src/index.js
+++ js-core/compiler/webpack/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
