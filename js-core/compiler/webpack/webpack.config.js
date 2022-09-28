9ec53b3ad0e9cf157c790d32550d320ee3c56bc5
HieuLuu <hieu.luu@binance.com>
2022-09-28 16:34:33 +0700

    feat: webpack development

--- js-core/compiler/webpack/webpack.config.js
+++ js-core/compiler/webpack/webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -15,5 +20,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
